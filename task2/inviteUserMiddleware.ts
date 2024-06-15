a4f2ad4e9b33f8bf8f4b8b208f2a12d0563965f2
Carlos Guerrero <git@carlosguerrero.com>
2024-06-15 16:17:01 +0100

    Task2 improved version

--- task2/inviteUserMiddleware.ts
+++ task2/inviteUserMiddleware.ts
@@ -1,54 +1,90 @@
 import superagent from "superagent";
 import { User, Shop } from "./models";
 
-export const inviteUser = function (req, res) {
-  var invitationBody = req.body;
-  var shopId = req.params.shopId;
-  var authUrl = "https://url.to.auth.system.com/invitation";
+const processInviteUser = async function (req) {
+  console.assert(req.body, "request body is required");
+  const invitationBody = req.body;
 
-  superagent
-    .post(authUrl)
-    .send(invitationBody)
-    .end(function (err, invitationResponse) {
-      if (invitationResponse.status === 201) {
-        User.findOneAndUpdate(
-          {
-            authId: invitationResponse.body.authId,
-          },
-          {
-            authId: invitationResponse.body.authId,
-            email: invitationBody.email,
-          },
-          {
-            upsert: true,
-            new: true,
-          },
-          function (err, createdUser) {
-            Shop.findById(shopId).exec(function (err, shop) {
-              if (err || !shop) {
-                return res
-                  .status(500)
-                  .send(err || { message: "No shop found" });
-              }
-              if (
-                shop.invitations.indexOf(invitationResponse.body.invitationId)
-              ) {
-                shop.invitations.push(invitationResponse.body.invitationId);
-              }
-              if (shop.users.indexOf(createdUser._id) === -1) {
-                shop.users.push(createdUser);
-              }
-              shop.save();
-            });
-          }
-        );
-      } else if (invitationResponse.status === 200) {
-        res.status(400).json({
-          error: true,
-          message: "User already invited to this shop",
-        });
-        return;
+  console.assert(req.params.shopId, "shopId is required");
+  console.assert(invitationBody.email, "email is required");
+
+  if (!req.params.shopId || !invitationBody.email) {
+    return {
+      status: 400,
+      body: { message: "shopId and email are required" },
+    };
+  }
+
+  const shopId = req.params.shopId;
+  const authUrl = "https://url.to.auth.system.com/invitation";
+
+  try {
+    // First: verify that external auth system processes the request correctly
+    let invitationResponse = await superagent
+      .post(authUrl)
+      .send(invitationBody);
+
+    // If auth system responds with 201, create/find user in our system and add to shop
+    if (invitationResponse.status === 201) {
+      
+      // Create user if not exists
+      const createdUser = await User.findOneAndUpdate(
+        {
+          authId: invitationResponse.body.authId,
+        },
+        {
+          authId: invitationResponse.body.authId,
+          email: invitationBody.email,
+        },
+        {
+          upsert: true,
+          new: true,
+        }
+      );
+
+      // Get shop and add user to shop
+      const shop = await Shop.findById(shopId);
+      if (!shop) {
+        return {
+          status: 404,
+          body: { message: "Shop not found" },
+        };
+      } else {
+        // Add user to shop
+        if (shop.invitations.indexOf(invitationResponse.body.invitationId)) {
+          shop.invitations.push(invitationResponse.body.invitationId);
+        }
+        if (shop.users.indexOf(createdUser._id) === -1) {
+          shop.users.push(createdUser);
+        }
+        await shop.save();
+        return {
+          status: 200,
+          body: { message: "Invited to shop" },
+        };
       }
-      res.json(invitationResponse);
-    });
+    } else if (invitationResponse.status === 200) {
+      // If auth system responds with 200, user already invited to shop
+      return {
+        status: 400,
+        body: { message: "User already invited to this shop" },
+      };
+    } else {
+      // If auth system responds with other status code, return error
+      return {
+        status: 500,
+        body: { message: "unhandled InvitationResponse" },
+      };
+    }
+  } catch (error) {
+    return {
+      status: 500,
+      body: { message: "unhandled Error", error: error },
+    };
+  }
+};
+
+export const inviteUser = async function (req, res) {
+  const response = await processInviteUser(req);
+  res.status(response.status).json(response.body);
 };
